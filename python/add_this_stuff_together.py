6c2554281a4c60bc7548d54ca600b39633462f3b
olouie <olivia.louie@gmail.com>
2016-03-09 21:05:02 -0800

    first commit

---
+++ python/add_this_stuff_together.py
@@ -0,0 +1,24 @@
+# Add a = [5,6,3] and b = [9, 9, 9] should create an array c of c = [1,5,6,2]
+
+a = [5, 6, 3]
+b = [9, 9, 9]
+
+def add_these(array1, array2):
+	d = [0]
+	c = []
+
+	while array1:
+
+		total = array1.pop() + array2.pop() + d.pop()
+
+		for i in str(total):
+			d.append(int(i))
+
+		c.insert(0, d.pop())
+
+		if not a:
+			c.insert(0, d.pop())
+
+	print c
+
+add_these(a,b)
